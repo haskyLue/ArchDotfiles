da9ff8d0b330b74c04e251f3c9d1b587b63741ef
haskyLue <ludebing@gmail.com>
2015-01-21 15:21:58 +0800

    .

--- bashrc/functions.sh
+++ bashrc/functions.sh
@@ -187,39 +187,39 @@ Ugitdir(){
 # 	sudo modprobe -fv ath9k
 # }
 
-extract() {
-    local c e i
-
-    (($#)) || return
-
-    for i; do
-        c=''
-        e=1
-
-        if [[ ! -r $i ]]; then
-            echo "$0: file is unreadable: \`$i'" >&2
-            continue
-        fi
-
-        case $i in
-            *.t@(gz|lz|xz|b@(2|z?(2))|a@(z|r?(.@(Z|bz?(2)|gz|lzma|xz)))))
-                   c=(bsdtar xvf);;
-            *.7z)  c=(7z x);;
-            *.bz2) c=(bunzip2);;
-            *.exe) c=(cabextract);;
-            *.gz)  c=(gunzip);;
-            *.rar) c=(unrar x);;
-            *.xz)  c=(unxz);;
-            *.zip) c=(gbkunzip);;
-            *)     echo "$0: unrecognized file extension: \`$i'" >&2
-                   continue;;
-        esac
-
-        command "${c[@]}" "$i"
-        ((e = e || $?))
-    done
-    return "$e"
-}
+# extract() {
+#     local c e i
+#
+#     (($#)) || return
+#
+#     for i; do
+#         c=''
+#         e=1
+#
+#         if [[ ! -r $i ]]; then
+#             echo "$0: file is unreadable: \`$i'" >&2
+#             continue
+#         fi
+#
+#         case $i in
+#             *.t@(gz|lz|xz|b@(2|z?(2))|a@(z|r?(.@(Z|bz?(2)|gz|lzma|xz)))))
+#                    c=(bsdtar xvf);;
+#             *.7z)  c=(7z x);;
+#             *.bz2) c=(bunzip2);;
+#             *.exe) c=(cabextract);;
+#             *.gz)  c=(gunzip);;
+#             *.rar) c=(unrar x);;
+#             *.xz)  c=(unxz);;
+#             *.zip) c=(gbkunzip);;
+#             *)     echo "$0: unrecognized file extension: \`$i'" >&2
+#                    continue;;
+#         esac
+#
+#         command "${c[@]}" "$i"
+#         ((e = e || $?))
+#     done
+#     return "$e"
+# }
 calc() {
     echo "scale=3;$@" | bc -l
 }
--- script/create_ramdisk_osx.sh
+++ script/create_ramdisk_osx.sh
@@ -1,25 +1,37 @@
 #! /bin/bash
-secret="/Users/hasky/Documents/secret"
+# 强迫症患者
 mount_point=/Volumes/Caches
 create_ramdisk(){
-	# copied from web 
 	ramfs_size_mb=512  
 
 	ramfs_size_sectors=$(( ${ramfs_size_mb}*1024*1024/512 ))  
 	ramdisk_dev=`hdid -nomount ram://${ramfs_size_sectors}`  
 	newfs_hfs ${ramdisk_dev}  
-	sudo -S mkdir -p ${mount_point}  < $secret
-	sudo -S mount -o noatime -t hfs ${ramdisk_dev} ${mount_point}  < $secret
-
-	echo ""
-	echo "-------remove with----------"  
-	echo "hdiutil detach ${ramdisk_dev}" 
-	echo ""
+	mkdir -p ${mount_point}
+	mount -o noatime,noowners -t hfs ${ramdisk_dev} ${mount_point} 
+	# echo ""
+	# echo "-------remove with----------"  
+	# echo "hdiutil detach ${ramdisk_dev}" 
+	# echo ""
 }
 
 cache_to_ram(){
+	_dir=(
+	Google
+	com.apple.Safari
+	Firefox
+	com.xiami.client
+	U_L_Logs
+	R_L_Logs
+	Homebrew
+	com.apple.dashboard.client
+	)
 	if [ ! -d $mount_point/Google ] ;then
-		sudo -S mkdir -v $mount_point/{Google,com.apple.Safari,Firefox,com.xiami.client,Logs,sys_Logs,Homebrew,com.apple.dashboard.client} < $secret
+		cd $mount_point 
+		for i in ${_dir[@]};do
+			mkdir -pv $i 
+		done
+		[[ ! -L /var/log ]] && mv /var/log var_log && ln -sf $mount_point/var_log /var/log || rm -f /var/log # /var/log 在此脚本直接生成
 	fi
 }
 ram_restoreto_cache(){
@@ -29,11 +41,10 @@ ram_restoreto_cache(){
 		hdiutil detach $_ramdisk_dev
 	fi
 }
-if [ $# -gt 0 ];then
-	ram_restoreto_cache
-	df -a
-else
-	create_ramdisk
-	cache_to_ram 
-	df -a
-fi
+# if [ $# -gt 0 ];then
+# 	ram_restoreto_cache
+# 	df -a
+# else
+create_ramdisk && cache_to_ram 
+# 	df -a
+# fi
